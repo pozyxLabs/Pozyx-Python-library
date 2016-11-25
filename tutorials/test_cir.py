86e0e2e58d4781ae39bf62b5d055529025cda365
Nicolas AMIOT <amiot.nicolas@gmail.com>
2016-11-25 14:28:04 +0100

    work cir

--- tutorials/test_cir.py
+++ tutorials/test_cir.py
@@ -9,6 +9,10 @@ You can find the Arduino tutorial here:
 """
 from pypozyx import *
 
+from pypozyx.definitions.registers import *
+
+
+
 port = '/dev/ttyACM1'                # COM port of the Pozyx device
 
 remote_id = 0x605D           # the network ID of the remote device
@@ -21,3 +25,16 @@ range_step_mm = 1000         # distance that separates the amount of LEDs lighti
 
 p = PozyxSerial(port)
 
+
+
+
+param = Data([0x0000,49],'Hi')
+
+
+
+data_format = ['i']*98
+data_format = ''.join(data_format)
+cir = Data([],data_format)
+
+
+p.useFunction(POZYX_CIR_DATA, params, cir, remote_id)
