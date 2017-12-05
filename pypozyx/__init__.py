30129e095986878b7a7a0c2a6b61fc32f457038a
Laurent Van Acker <laurent@pozyx.io>
2017-12-05 11:56:56 +0100

    Additional version bump for fixing readmes.

--- pypozyx/__init__.py
+++ pypozyx/__init__.py
@@ -80,7 +80,7 @@ switch at some point.
 
 """
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 VERSION = __version__
 version = __version__
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='pypozyx',
     packages=['pypozyx', 'pypozyx.definitions',
               'pypozyx.structures'],
-    version='1.1.1',
+    version='1.1.2',
     description='Python library for Pozyx devices',
     install_requires=[
         'pyserial>=3.0'
